c3e9e2615900c0318302d245aaffc6ea966acd48
vitozs <vitornuneschagas2016@gmail.com>
2023-10-12 20:48:06 -0300

    feat: teste da classe Calculadora

--- src/test/java/com/br/projeto/calculadora/CalculadoraTest.java
+++ src/test/java/com/br/projeto/calculadora/CalculadoraTest.java
@@ -10,10 +10,23 @@ public class CalculadoraTest {
 
     @DisplayName("Testa se retorna a distancia certa entre as cidades")
     @Test
-    void getDistanciaTest(){
-        int distancia = calculadora.getDistanciaEntreCidades("natal", "belem").intValue();
+    void getDistanciaEntreCidadesTest(){
+        calculadora.setCidade("natal");
+        calculadora.setDestino("belem");
+        int distancia = calculadora.getDistanciaEntreCidades().intValue();
 
         Assert.assertEquals(2108, distancia);
+
+    }
+
+    @DisplayName("Testa se retorna o valor correto de custo p/km")
+    @Test
+    void valorTotalTest(){
+        calculadora.setCidade("natal");
+        calculadora.setDestino("belem");
+        calculadora.setTamanhoCaminhao("grande");
+
+        Assert.assertEquals(61574.68, calculadora.valorTotal(), 0);
 
     }
 
