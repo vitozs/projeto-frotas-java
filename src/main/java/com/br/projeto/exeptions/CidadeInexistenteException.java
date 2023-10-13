80d2dd061713218f3890dfe53aa36d16df046a52
Vitor Nunes Chagas <94878000+vitozs@users.noreply.github.com>
2023-10-13 15:49:45 -0300

    feat: exception para a cidade invalida

--- src/main/java/com/br/projeto/exeptions/CidadeInexistenteException.java
+++ src/main/java/com/br/projeto/exeptions/CidadeInexistenteException.java
@@ -3,6 +3,6 @@ package com.br.projeto.exeptions;
 public class CidadeInexistenteException extends RuntimeException {
     public CidadeInexistenteException (String mensagem){
         super(mensagem);
-    }
+    } 
 
 }
